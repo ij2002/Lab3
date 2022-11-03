659eb336c194c80b96a712d6a2a86416b6c3b382
ij2002 <109142734+ij2002@users.noreply.github.com>
2022-11-03 05:24:58 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
+$(document).ready(function(){
+    // exercise 1
+    $('p').on('click', function () {
+        $(this).hide();
+    })
 
+    //exercise 2 
+    //part 1
+    $('body').prepend('<div id="newdiv"><h1>Welcome!</h1></div>');
+
+    //part 2
+    $('#ex2').append('<div class="yellow">1st yellow div</div>');
+    $('#ex2').append('<div class="yellow">2nd yellow div</div>');
+    $('.yellow').css("background-color", "yellow");
+
+    //part 3
+    $('#hide').on('click', function () {
+        $('#show_hide').hide();
+    });
+    $('#show').on('click', function () {
+        $('#show_hide').show();
+    });
+
+    //part 4
+    $('#toggle').on('click', function () {
+        $('#toggle_text').toggle();
+    });
+    
+    //part 5
+    $('#name').on('click', function () {
+        $('body').append('<div>Ishana Jabbar</div>');
+    });
+});
